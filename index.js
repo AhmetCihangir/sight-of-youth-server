02d12b9691deb49d8d3ffeacb2c7fab6406b2d6e
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-08-26 18:01:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,21 @@ var corsOptionsDelegate = function (req, callback) {
 }
 
 
-app.all("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  return next();
-});
-
-app.all("/*", function(req, res, next) {
-  if (req.method.toLowerCase() !== "options") {
-    return next();
-  }
-  return res.send(204);
-});
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.options(
+  '*',
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 
 
 //app.use(cors(corsOptionDelegate))
