7ccdb16ec83142cbcf17eec474dbd5316eb4efa3
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-08-26 17:43:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ var corsOptions = {
 
 
 
-app.use(cors({
-  origin : "*"
-}))
+//app.use(cors({origin : "*"}))
 
 //const allowCrossDomain = function(req, res, next) {
 //  res.header('Access-Control-Allow-Origin', "*");
@@ -35,8 +33,19 @@ app.use(cors({
 //  res.header('Access-Control-Allow-Headers', 'Content-Type');
 //  next();
 //}
+app.use((req, res, next) => { //doesn't send response just adjusts it
+    res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+        return res.status(200).json({});
+    }
+    next(); //so that other routes can take over
+})
 
-//app.use(allowCrossDomain)
 
 app.use(bodyParser.json())
 
