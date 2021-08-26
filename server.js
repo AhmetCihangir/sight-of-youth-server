a04a1b60cf0efdd0f0ca7e43526b246b55f458d1
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-08-26 17:35:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var corsOptions = {
 //app.use(cors(corsOptions))
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "https://sight-of-youth.vercel.app");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
