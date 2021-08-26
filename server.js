338acd688d499b48f3a82025b7fc39874e0caf3c
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-08-26 17:37:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,16 +25,16 @@ var corsOptions = {
 
 
 
-//app.use(cors(corsOptions))
+app.use(cors())
 
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
+//const allowCrossDomain = function(req, res, next) {
+//  res.header('Access-Control-Allow-Origin', "*");
+//  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//  res.header('Access-Control-Allow-Headers', 'Content-Type');
+//  next();
+//}
 
-app.use(allowCrossDomain)
+//app.use(allowCrossDomain)
 
 app.use(bodyParser.json())
 
